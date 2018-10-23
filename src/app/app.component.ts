b82c3a4712f288fc9052998b135fcc9e5689aa50
siddik49 <muhammed.s@spurtreetech.in>
2018-10-23 13:11:04 +0530

    example changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'martjack example';
+  searchQuery: string;
+  productSearchResult: any = null;
+  errr: any = null;
+
+  constructor(private _http:HttpClient) {
+    
+  }
+
+
+  private searchProducts() {
+
+    let productRes = this.getProductsBySearch(this.searchQuery);
+    productRes.subscribe(res => {
+      console.log(res, "success response from martjack");
+      this.productSearchResult = res.resource;
+      alert('success');
+    }, err => {
+      console.log(err, "error");
+      this.errr = JSON.parse(JSON.stringify(err.message));
+      alert('failure');
+    })
+    
+  }
+
+
+
+  getProductsBySearch(queryString: string): Observable<any> {
+    
+    console.log(queryString, "queryString");
+
+    const url = `products?search-text=${queryString}`;
+    const finalUrl = `https://sg-frontapi.ecom.capillary.in/v3/f3537938-adfd-45db-b173-4aaad83b4edd/${url}`;
+    return this._http.get<any>(finalUrl);
+
+  }
 }
